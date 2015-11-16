31006b6d13ccdd1077c6b8d0b26fb195b1bc67d0
Marvin Bertin <marvin.bertin@gmail.com>
2015-11-16 12:22:10 -0500

    Regression

--- src/main/java/org/deeplearning4j/examples/Marvin/regression/RegressionExample.java
+++ src/main/java/org/deeplearning4j/examples/Marvin/regression/RegressionExample.java
@@ -5,6 +5,7 @@ import org.canova.api.records.reader.impl.CSVRecordReader;
 import org.canova.api.split.FileSplit;
 import org.deeplearning4j.datasets.canova.RecordReaderDataSetIterator;
 import org.deeplearning4j.datasets.iterator.DataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
@@ -14,16 +15,21 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.optimize.solvers.LBFGS;
+import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.api.ops.impl.transforms.Identity;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 
 /**
  * Created by marvinbertin on 11/13/15.
  */
 public class RegressionExample {
+
+    private static Logger log = LoggerFactory.getLogger(RegressionExample.class);
 
     public static void main(String[] args) throws Exception {
         int seed = 3;
@@ -34,6 +40,9 @@ public class RegressionExample {
                 new FileSplit(
                         new ClassPathResource("regression-example.txt")
                                 .getFile()));
+
+        DataSetIterator iter = new RecordReaderDataSetIterator(reader,null,2029,12,1,true);
+        DataSet next = iter.next();
 
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .seed(seed)
@@ -51,8 +60,6 @@ public class RegressionExample {
                 )
                 .build();
 
-        DataSetIterator iter = new RecordReaderDataSetIterator(reader,null,2029,12,1,true);
-        DataSet next = iter.next();
         next.normalizeZeroMeanZeroUnitVariance();
         SplitTestAndTrain testAndTrain = next.splitTestAndTrain(0.9);
 
@@ -61,6 +68,11 @@ public class RegressionExample {
         network.setListeners(new ScoreIterationListener(50));
         network.fit(testAndTrain.getTrain());
 
+        Evaluation eval = new Evaluation(2);
+        DataSet test = testAndTrain.getTest();
+        INDArray output = network.output(test.getFeatureMatrix());
+        eval.eval(test.getLabels(), output);
+        log.info(eval.stats());
 
 
     }
